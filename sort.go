bee4b92c4e8c412a3fa2acab7ac7f6c02038fe40
Arun CS <aruncs31ss@gmail.com>
2026-08-11 15:18:42 +0530

    added sort

---
+++ sort.go
@@ -0,0 +1,17 @@
+package responsehelper
+
+import (
+	"slices"
+	"strings"
+)
+
+type WithName interface {
+	String() string
+}
+
+func Sort[T WithName](in []T) []T {
+	slices.SortFunc(in, func(a, b T) int {
+		return strings.Compare(a.String(), b.String())
+	})
+	return in
+}
